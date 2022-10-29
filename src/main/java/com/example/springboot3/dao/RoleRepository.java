f599f0d796c3bd73606203b9e4747e7bd6d39b14
alikkaribov <alik_karibov@mail.ru>
2022-10-29 16:34:51 +0400

    Initial commit

---
+++ src/main/java/com/example/springboot3/dao/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.springboot3.dao;
+
+import com.example.springboot3.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByname(String name);
+}
---
+++ src/main/java/com/example/springboot3/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.springboot3.dao;
+
+import com.example.springboot3.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByusername(String username);
+}
